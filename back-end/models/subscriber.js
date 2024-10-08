461bdf119d8972a8b1bd7526a9f32ee0c32efa02
Aryan Amish <aryan.amish@zohocorp.com>
2024-10-08 19:57:24 +0530

    Db design

---
+++ back-end/models/subscriber.js
@@ -0,0 +1,11 @@
+/**
+ * name: string (optional)
+ * email: string (optional)
+ * emirateID: string (optional)
+ * mobile: number (required)
+ * comment: string (optional)
+ * 
+ *   isContacted - boolean
+ *  isPaid - boolea
+ * 
+ */
