0a3dbeb3228a880ff398f978926903bed9774289
Aryan Amish <aryanamish385@gmail.com>
2024-10-22 12:38:11 +0530

    Changed backend URL

--- front-end/src/graphQL/apolloClient.tsx
+++ front-end/src/graphQL/apolloClient.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/api',
+  uri: '/api',
   cache: new InMemoryCache(),
 });
 
