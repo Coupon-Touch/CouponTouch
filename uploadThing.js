ecddb20688f8d62b62e47e5ad104498a8be9fe64
Aryan Amish <aryanamish385@gmail.com>
2024-11-14 00:38:23 +0530

    Update uploadThing.js

--- uploadThing.js
+++ uploadThing.js
@@ -5,21 +5,25 @@ const f = createUploadthing();
 export const uploadRouter = {
   logoUpload: f({
     image: { maxFileSize: '100KB', maxFileCount: 1, minFileCount: 1 },
-  }),
-  // .middleware(({ req }) => auth(req))
-  // .onUploadComplete(data => {}),
+  })
+    // .middleware(({ req }) => auth(req))
+    .onUploadComplete(data => {}),
+
   // Scratch Card Background route - allows PNG, JPEG, WEBP with max file size of 500KB
   scratchCardBackground: f({
     image: { maxFileSize: '500KB', maxFileCount: 1, minFileCount: 1 },
-  }),
-  // .middleware(({ req }) => auth(req))
-  // .onUploadComplete(data =>{}),
+  })
+    // .middleware(({ req }) => auth(req))
+    .onUploadComplete(data =>
+      console.log('Scratch Card Background uploaded', data)
+    ),
+
   // Prize Image upload route - allows PNG, JPEG, WEBP with max file size of 1MB
   prizeImage: f({
     image: { maxFileSize: '1MB', maxFileCount: 1, minFileCount: 1 },
-  }),
-  // .middleware(({ req }) => auth(req))
-  // .onUploadComplete(data =>{}),
+  })
+    // .middleware(({ req }) => auth(req))
+    .onUploadComplete(data => {}),
 };
 
 // export type OurFileRouter = typeof uploadRouter;
